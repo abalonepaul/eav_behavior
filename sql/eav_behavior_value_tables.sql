643069f108f07962fb234afcc46a4f1511f00145
Paul Marshall <paul@protelligence.com>
2011-10-11 08:52:26 -0700

    Updated Readme. Added SQL files

---
+++ sql/eav_behavior_value_tables.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 10, 2011 at 10:24 AM
+-- Server version: 5.5.9
+-- PHP Version: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `eav_behavior`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_binary_values`
+--
+
+CREATE TABLE `attributes_binary_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_boolean_values`
+--
+
+CREATE TABLE `attributes_boolean_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_datetime_values`
+--
+
+CREATE TABLE `attributes_datetime_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_date_values`
+--
+
+CREATE TABLE `attributes_date_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` date DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_float_values`
+--
+
+CREATE TABLE `attributes_float_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` float DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_integer_values`
+--
+
+CREATE TABLE `attributes_integer_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` int(11) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_key_values`
+--
+
+CREATE TABLE `attributes_key_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` int(11) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_string_values`
+--
+
+CREATE TABLE `attributes_string_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_text_values`
+--
+
+CREATE TABLE `attributes_text_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` text,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_timestamp_values`
+--
+
+CREATE TABLE `attributes_timestamp_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_time_values`
+--
+
+CREATE TABLE `attributes_time_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` time DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attributes_uuid_values`
+--
+
+CREATE TABLE `attributes_uuid_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entity_id` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `value` char(36) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entity_id` (`entity_id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
